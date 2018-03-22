67f8c80f0a5bb613327db12ebf29dbbbedf4e4bb
usr_icamargo <junior.camargo11@gmail.com>
2018-03-22 15:53:48 -0300

    commit final

---
+++ src/components/Catalan/Carousel/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Carousel as AntCarousel, Col, Button, Avatar } from 'antd';
+import axios from 'axios';
+
+class Carousel extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            avatars: [],
+        };
+
+        this.carousel = null;
+    }
+
+    componentDidMount() {
+        axios({
+            method: 'get',
+            url: 'https://nutror-mock-server.herokuapp.com/catalan/images',
+        }).then(
+            res => this.setState({ avatars: res.data.imageUrl }),
+            err => console.error(err),
+        );
+    }
+
+    handleNext = () => {
+        this.carousel.next();
+    }
+
+    handlePrev = () => {
+        this.carousel.prev();
+    }
+
+    render() {
+        return (
+            <div>
+                <AntCarousel
+                    dots={false}
+                    effect='fade'
+                    ref={el => this.carousel = el}
+                >
+                    {
+                        this.state.avatars.length === 0 ?
+                            <Col className='avatar'>
+                                <Avatar src='https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif' />
+                            </Col>
+                        :
+                            this.state.avatars.map(avatar => (
+                                <Col className='avatar'>
+                                    <Avatar src={avatar} />
+                                </Col>
+                            ))
+                    }
+                </AntCarousel>
+                <div className="skills">
+                    <Col span={24} className="skills-itens">
+                        <Button onClick={this.handlePrev}><i className="anticon anticon-caret-left"></i></Button> 
+                        <Button onClick={this.handleNext}><i className="anticon anticon-caret-right"></i></Button>         
+                    </Col>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Carousel;
--- src/components/Catalan/index.js
+++ src/components/Catalan/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import Carousel from './Carousel';
 
-import { Row, Col, Avatar, Button } from "antd";
+import { Row } from "antd";
 
 import { Helmet } from "react-helmet";
 
@@ -12,15 +12,7 @@ const Catalan = () => {
                 <title>Catalan</title>
             </Helmet>
             <Row>
-                    <Col className="avatar" span={24}>
-                    <Avatar src="http://img.kbhgames.com/2014/09/Dragon-Ball-Advanced-Adventure.jpg" />
-                    </Col>
-                    <div className="skills">
-                    <Col span={24} className="skills-itens">
-                    <Button><i class="anticon anticon-caret-left"></i></Button> 
-                    <Button><i class="anticon anticon-caret-right"></i></Button>         
-                    </Col>
-                    </div>
+                <Carousel />
             </Row>
         </div>
    )
